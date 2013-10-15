330d3f529467b883f8cfa30d4cf5780496718ac2
ydderd <dhanvi.reddy@tapjoy.com>
2013-10-15 10:47:45 -0400

    New active record extensions file to house backports; a new method to delete a single association from the association cache:

---
+++ lib/identity_cache/active_record_extension.rb
@@ -0,0 +1,69 @@
+module ActiveRecord
+  module Serialization
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def initialize_attributes(attributes, options = {}) #:nodoc:
+        serialized = (options.delete(:serialized) { true }) ? :serialized : :unserialized
+        #super(attributes, options)
+
+        serialized_attributes.each do |key, coder|
+          if attributes.key?(key)
+            attributes[key] = Attribute.new(coder, attributes[key], serialized)
+          end
+        end
+
+        attributes
+      end
+    end
+
+  end
+end
+
+module ActiveRecord
+  module Associations
+    extend ActiveSupport::Concern
+
+    attr_reader :association_cache
+
+    private
+      # Set the specified association instance.
+      def delete_from_cache(name)
+        instance_variable_set "@#{name}", nil
+      end
+    end
+end
+
+module ActiveRecord
+  module Reflection
+    class AssociationReflection
+      extend ActiveSupport::Concern
+
+      
+      def association_class
+        case macro
+        when :belongs_to
+          if options[:polymorphic]
+            Associations::BelongsToPolymorphicAssociation
+          else
+            Associations::BelongsToAssociation
+          end
+        when :has_and_belongs_to_many
+          Associations::HasAndBelongsToManyAssociation
+        when :has_many
+          if options[:through]
+            Associations::HasManyThroughAssociation
+          else
+            Associations::HasManyAssociation
+          end
+        when :has_one
+          if options[:through]
+            Associations::HasOneThroughAssociation
+          else
+            Associations::HasOneAssociation
+          end
+        end
+      end
+    end
+  end
+end
